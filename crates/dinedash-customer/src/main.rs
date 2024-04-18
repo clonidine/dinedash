0847d6e3eb31ead3a984180ab331c7a6840091dc
clonidine <lcly@duck.com>
2024-04-18 22:23:40 +0000

    chore(dinedash-customer): add `repository` and `routes` modules

--- crates/dinedash-customer/src/main.rs
+++ crates/dinedash-customer/src/main.rs
@@ -2,6 +2,8 @@ use std::io;
 
 pub mod api;
 pub mod config;
+pub mod repository;
+pub mod routes;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
---
+++ crates/dinedash-customer/src/repository.rs
@@ -0,0 +1,15 @@
+pub async fn find() {
+    todo!()
+}
+
+pub async fn delete() {
+    todo!()
+}
+
+pub async fn get() {
+    todo!()
+}
+
+pub async fn insert() {
+    todo!()
+}
